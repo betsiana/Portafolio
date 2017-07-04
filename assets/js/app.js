becf37db17ef25cf0ce03eb771939a34934e6c43
Betsi Loayza <betsi18_12@hotmail.com>
2017-07-04 18:05:15 -0500

    inicio

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+// var menu = document.getElementsByClassName("nav")[0];
+// var tituloNav = document.getElementsByClassName("titulo")[0];
+// var barra = document.getElementsByTagName("li")[0];
+// var barra2 = document.getElementsByTagName("li")[1];
+// var barra3 = document.getElementsByTagName("li")[2];
+//
+// function scroll (event){
+//   var scroll=window.pageYOffset || document.body.scrollTop;
+//   console.log(scroll);
+//   if(scroll>3){
+//     menu.style.paddingTop = "12px";
+//     menu.style.paddingBottom = "12px";
+//     tituloNav.style.fontSize = "18px";
+//   }
+//   if(scroll>712){
+//      console.log(barra);
+//     barra.classList.add("pestana");
+//   }else{
+//     barra.classList.remove("pestana");
+//   }
+//   if(scroll>1566 ){
+//     barra2.classList.add("pestana");
+//     barra.classList.remove("pestana");
+//   }else{
+//     barra2.classList.remove("pestana");
+//   }
+//   if(scroll>2111){
+//     barra3.classList.add("pestana");
+//     barra2.classList.remove("pestana");
+//   }else{
+//     barra3.classList.remove("pestana");
+//   }
+//   if(scroll<3){
+//     menu.style.paddingTop = "25px";
+//     menu.style.paddingBottom = "25px";
+//     tituloNav.style.fontSize = "35px";
+//   }
+// }
+// window.addEventListener("scroll",scroll);
