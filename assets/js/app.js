cea41cd38ca4931eed0b2eb2f829d8370d3d129c
betsiana <betsi18_12@hotmail.com>
2017-07-13 00:17:13 -0500

    barra

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,20 +14,35 @@ $('#trabajo').on('click', function(){
   $('#contacto').removeClass("active");
 });
 
+// $(function(){
+//      $('a[href*=#]').click(function() {
+//      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+//          && location.hostname == this.hostname) {
+//             var $target = $(this.hash);
+//              $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+//              if ($target.length) {
+//                 var targetOffset = $target.offset().top;
+//                  $('html,body').animate({scrollTop: targetOffset}, 1000);
+//                return false;
+//             }
+//        }
+//    });
+// });
 
-
-
-$(function(){
-     $('a[href*=#]').click(function() {
-     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-         && location.hostname == this.hostname) {
-            var $target = $(this.hash);
-             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-             if ($target.length) {
-                var targetOffset = $target.offset().top;
-                 $('html,body').animate({scrollTop: targetOffset}, 1000);
-               return false;
-            }
-       }
-   });
-});
+$(document).ready(main);
+var cont = 1;
+function main(){
+	$('.barra-menu').click(function(){
+		if(cont == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			cont = 0;
+		} else {
+			cont = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+};
