d2662f4d2ba09f161a919d87605412e10d3e83d5
Betsi Loayza <betsi18_12@hotmail.com>
2017-07-13 18:39:27 -0500

    animacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,40 +9,8 @@ $('#contacto').on('click', function(){
   $('#mi').removeClass("active");
 });
 $('#trabajo').on('click', function(){
+  // $('.trabajos . titulo h3').addClass("fadeInUp");
   $('#trabajo').addClass("active");
   $('#mi').removeClass("active");
   $('#contacto').removeClass("active");
 });
-
-// $(function(){
-//      $('a[href*=#]').click(function() {
-//      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-//          && location.hostname == this.hostname) {
-//             var $target = $(this.hash);
-//              $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-//              if ($target.length) {
-//                 var targetOffset = $target.offset().top;
-//                  $('html,body').animate({scrollTop: targetOffset}, 1000);
-//                return false;
-//             }
-//        }
-//    });
-// });
-
-$(document).ready(main);
-var cont = 1;
-function main(){
-	$('.barra-menu').click(function(){
-		if(cont == 1){
-			$('nav').animate({
-				left: '0'
-			});
-			cont = 0;
-		} else {
-			cont = 1;
-			$('nav').animate({
-				left: '-100%'
-			});
-		}
-	});
-};
