db366a322fe01690c8ed2918b00dd06de56f732d
Betsi Loayza <betsi18_12@hotmail.com>
2017-07-14 14:38:32 -0500

    animaciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,33 @@ $('#contacto').on('click', function(){
   $('#mi').removeClass("active");
 });
 $('#trabajo').on('click', function(){
-  // $('.trabajos . titulo h3').addClass("fadeInUp");
   $('#trabajo').addClass("active");
   $('#mi').removeClass("active");
   $('#contacto').removeClass("active");
+});
+
+function flash (idPadre, elementoHijo){
+  idPadre.hover(function(){
+    elementoHijo.addClass('flash');
+    }, function(){
+      elementoHijo.removeClass('flash');
+    });
+}
+var obj = [1,2,3,4,5,6,7,8];
+$.each( obj, function( i, val ) {
+  flash($( "#btn" + i ), $('#btn'+ i+' '+'span')) ;
+});
+
+function pulse (id, imagen){
+  id.hover(function(){
+    imagen.addClass('pulse');
+    imagen.addClass('animated');
+    }, function(){
+      imagen.removeClass('pulse');
+      imagen.removeClass('animated');
+    });
+}
+var grupoImagen = [1,2,3,4,5,6,7,8,9,10,11];
+$.each( grupoImagen, function( i, val ) {
+  pulse($( "#icon" + i ), $('#icon'+ i+' '+'img')) ;
 });
